3310a914f62fa2ef39ec4297b009d234849930d7
Ryan McAndrew <rrmcandrew@gmail.com>
2024-06-09 14:31:38 -0400

    design updates/comp transfer

---
+++ src/core/audio/channel.py
@@ -0,0 +1,5 @@
+class AudioChannel:
+    def __init__(self):
+        self.audioManager = None
+        self.numChannels = None
+        self.outputDeviceIndex = 0
---
+++ src/lib/MasterAudio.py
@@ -0,0 +1,5 @@
+import pyaudio
+
+class MasterAudioChannel:
+    def __init__(self):
+        pass
